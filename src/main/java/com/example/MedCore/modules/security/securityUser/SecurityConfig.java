6aa7ad3cea8e945f49636cf213afb5ab27e56745
Dmitriy <Dmitriyyuzkov23@gmail.com>
2025-04-29 23:09:43 +0300

    update DoctorController

--- src/main/java/com/example/MedCore/modules/security/securityUser/SecurityConfig.java
+++ src/main/java/com/example/MedCore/modules/security/securityUser/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/v1/users/register",
                                 "/api/v1/users/login",
-                                "/api/v1/documents/create",
+                                "/api/v1/documents/createф",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/swagger-resources/**",
